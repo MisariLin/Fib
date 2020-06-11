4c8014ce5dac40b5383c60b6fc3493a9cd3c960e
林威 <linwei@znlhzl.com>
2020-06-11 21:18:48 +0800

    init

---
+++ lib/homepage.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:myapp/playground.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Row(
+        children: [
+          Container(
+            width: 100,
+            color: Colors.blue,
+          ),
+          Expanded(child: buildPlayground()),
+          Container(
+            color: Colors.red,
+            width: 100,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPlayground(){
+    return Stack(
+      children: [
+        Positioned(
+          width: 375,
+          height: 667,
+          top: 100,
+          left: 300,
+          child: Playground())
+      ],
+    );
+  }
+}
---
+++ lib/widget_gridview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WidgetGridView extends StatefulWidget {
+  @override
+  _WidgetGridViewState createState() => _WidgetGridViewState();
+}
+
+class _WidgetGridViewState extends State<WidgetGridView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
